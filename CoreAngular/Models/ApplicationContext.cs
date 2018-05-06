530eb75cc335efaded7e9bbcb825c3c11dc6e98a
Andrey Sinetskiy <sinetskiy@gmail.com>
2018-05-06 15:07:58 +0300

    init 2

---
+++ CoreAngular/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreAngular.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CoreAngular/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using CoreAngular.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreAngular
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=productsdb;AttachDbFilename=C:\\Source\\CoreAngular\\CoreAngular\\AppData\\productsdb.mdf;Trusted_Connection=True;";
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // добавляем сборку через webpack
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
